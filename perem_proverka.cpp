3e534a45aa92db9ebde61ee636e8a692a3121050
DolgorukovAndrey <a88218302@gmail.com>
2024-12-15 18:58:42 +0500

    function

--- perem_proverka.cpp
+++ perem_proverka.cpp
@@ -6,18 +6,18 @@ using namespace std;
 
 void perem(int(&a)[4][4]) {
     srand(time(0));
-    int* ind = new int[16];
+    int* ind = new int[16]; // 1D array from 0 to 15
     for (int i = 0; i < 16; ++i) {
         ind[i] = i;
     }
-    for (int i = 15; i >= 0; --i) {
+    for (int i = 15; i >= 0; --i) { // mixing ind
         int j = rand() % (i + 1);
         int t = ind[i];
         ind[i] = ind[j];
         ind[j] = t;
     }
     int o = 0;
-    for (int i = 0; i < 4; ++i) {
+    for (int i = 0; i < 4; ++i) { // filling 2D array
         for (int j = 0; j < 4; j++) {
             a[i][j] = ind[o];
             o += 1;
@@ -27,17 +27,17 @@ void perem(int(&a)[4][4]) {
 }
 bool proverka(int(&a)[4][4]) {
     bool result = false;
-    int k = 0, s; // k - кол-во инверсий, s - номер строки с пустой €чейкой
-    int* ind = new int[16]; // одномерна€ копи€ двумерного массива
+    int k = 0, s; // k - count of inversions, s - counting inversions
+    int* ind = new int[16]; // a 1D copy of a 2D array
     int o = 0;
     for (int i = 0; i < 4; i++)
         for (int j = 0; j < 4; j++) {
             ind[o] = a[i][j];
             o += 1;
-            if (a[i][j] == 0) // поиск s
+            if (a[i][j] == 0) // searching s
                 s = i;
         }
-    for (int i = 0; i < 16; i++) // подсчет инверсий
+    for (int i = 0; i < 16; i++) // counting inversions
     {
         int n = ind[i];
         for (int j = i + 1; j < 16; j++)
@@ -48,7 +48,7 @@ bool proverka(int(&a)[4][4]) {
         }
     }
     delete[] ind;
-    if ((k % 2 == 0 && s % 2 != 0) || (k % 2 != 0 && s % 2 == 0)) // услови€ на решаемость
+    if ((k % 2 == 0 && s % 2 != 0) || (k % 2 != 0 && s % 2 == 0)) // conditions for solvability
         result = true;
     else
         result = false;
